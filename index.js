fd9368fd2bb76a7bd8389dcab3d8944fc4ebfe11
chrisngili <ngilichristopher@gmail.com>
2025-03-15 18:16:20 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.querySelector('#main')
+main.remove()
+
+const newHeader = document.createElement("h1")
+console.log(newHeader)
+
+newHeader.id = 'victory'
+console.log(newHeader)
+
+newHeader.textContent = "Justus is the champion"
